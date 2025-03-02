b3a253db8ea3346946f21f69e5bca93d0624d9bc
Tushar Jaiswal <tusharjaiswaltj01@gmail.com>
2025-03-02 17:04:40 +0530

    adding weather functionalities

---
+++ weather-app/app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Nashik",
+    country: "India",
+    state: "Maharashtra",
+    lat: 73.7898,
+    lon: 19.9975,
+  },
+  {
+    name: "Indore",
+    country: "India",
+    state: "Madhya pradesh",
+    lat: 75.8577,
+    lon: 22.7196,
+  },
+  {
+    name: "Delhi",
+    country: "India",
+    state: "Delhi",
+    lat: 77.1025,
+    lon: 28.7041,
+  },
+  {
+    name: "Kolkata",
+    country: "India",
+    state: "West Bengal",
+    lat: 88.3629,
+    lon: 22.5744,
+  },
+  {
+    name: "Bhopal",
+    country: "India",
+    state: "Madhya Pradesh",
+    lat: 77.4126,
+    lon: 23.2599,
+  },
+];
+
+export default defaultStates;
